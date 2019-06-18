737712cec24e7c935f3417af5ed9c169da0ea32c
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-18 19:58:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ app = Client("663574960:AAGWfrBnjGYGSczuGHGLG60RVLMp6ebWteM",665563,"7f2c2d12880
 
           
 @app.on_message(Filters.command("delete"))
-def forawrd(client, message):
- client.delete_messages(message.chat.id, range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2]))
+def main(client, message):
+   client.delete_messages(message.chat.id, range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2]))
 
   
 
