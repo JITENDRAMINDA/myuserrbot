60fa12b3372b3c678f36bb02b7fe17c540387689
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-18 20:48:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,11 @@ app = Client("663574960:AAGWfrBnjGYGSczuGHGLG60RVLMp6ebWteM", 605563,"7f2c2d1288
           
 @app.on_message(Filters.command("delete"))
 def main(client, message):
-   client.delete_messages(message.chat.id, range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])))
-
-  
+  for message in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
+    try:
+        client.delete_messages(message.chat.id,message)
+    except:
+        continue
 
 
 app.run()
