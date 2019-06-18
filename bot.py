ecf843cd785eab3cd5c555ad7caa929d99afa78c
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-18 19:38:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,12 @@
 from pyrogram import Client, Filters
 
 
-app = Client('666639160:AAEtopjBMU5r_i_UROx8PTWSSOtIrU6V7W8',605563,"7f2c2d12880400b88764b9b304e14e0b")
+app = Client(663574960:AAGWfrBnjGYGSczuGHGLG60RVLMp6ebWteM,665563,"7f2c2d12880400b88764b9b304e14e0b")
 
-
-
-u = '-1001394666637'
-s = '-1001274887387'
-
-
-@app.on_message(Filters.chat(int(s)) & Filters.text & ~ Filters.edited)
-
+          
+@app.on_message(Filters.command("delete"))
 def forawrd(client, message):
+ client.delete_messages(message.chat.id, range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2]))
 
-    mes = client.send_message(int(u), "**" + message.text + "**")
-    file = open("sure.txt" , "r")
-    lines = file.readlines()
-    file.close()
-    for line in lines:
-         files = open("sure.txt" , "w")
-         files.write( line + " " + str(message.message_id) +  " " + str(mes.message_id))
-         files.close()       
-
-
-
-
-@app.on_message(Filters.chat(int(s)) & Filters.sticker)
-
-def forawrd(client, message):
-
-   client.send_sticker(int(u),message.sticker.file_id)
-            
-@app.on_message(Filters.chat(int(s)) & Filters.text & Filters.edited)
-
-def forawrd(client, message):
-
-  file = open("sure.txt" , "r")
-  lines = file.readlines()
-  file.close()
-  for line in lines:
-    x = line.split()
-    id = str(message.message_id)
-    if id in x:
-      client.edit_message_text(int(u),int(x[x.index(id)+1]), "**" + message.text + "**" )
-
-
-
+  
 app.run()
