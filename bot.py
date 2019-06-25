b7b7ec9f81aad6f5ced0827088799713871c1e62
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-25 09:54:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,13 @@
 from pyrogram import Client, Filters
 
+app = Client("mcc",715451,"d2cba6f7bf5d1a45682da5bb9071a307")
 
-app = Client("663574960:AAGWfrBnjGYGSczuGHGLG60RVLMp6ebWteM", 605563,"7f2c2d12880400b88764b9b304e14e0b")
-
-          
-@app.on_message(Filters.command("add"))
+@app.on_message(Filters.command("delete"))
 def main(client, message):
-  file = open("sure.txt" , "r")
-  lines = file.readlines()
-  file.close()
-  for q in lines:
-    p = q.split()
-    message.reply((p + "☣️ ["+ message.reply_to_message.text+ "]" + "(" + message.text.split(' ')[1] + ")")for p in lines)
-  
-
+ for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
+  try:
+   client.delete_messages(message.chat.id,message.message_id)
+  except:
+   continue
 
 app.run()
