d980eefb1eb2ae69787883af3754dd218cc37bd9
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-28 17:06:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, Filters
 import random
-app = Client("icc",715451,"d2cba6f7bf5d1a45682da5bb9071a307")
+app = Client("ici",605563,"7f2c2d12880400b88764b9b304e14e0b")
 
 
 
