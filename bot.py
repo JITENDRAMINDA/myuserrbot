2457bea52844fcbaee498a7b092e7ab50714d952
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-06-28 18:30:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,6 @@ from pyrogram import Client, Filters
 import random
 app = Client("ici",605563,"7f2c2d12880400b88764b9b304e14e0b")
 
-@app.on_message(Filters.command("g"))
-def main(client, message):
- if message.from_user.id == 491634139:
-    client.send_message(
-    chat_id=message.chat.id,
-    text=random.choice(["Teri maa ki chut maderchod","mkc","bahan ke loude","Behenchod","Teri maa ko chodu","Aaukat se bahar ja rha hai","Tere jese to me gaand dhone me rakhta hu","Gand mara mkc teri" ,"Randy ki oald","Teri maiya ka bhoada","Maderchod"]),
-    reply_to_message_id=message.reply_to_message.message_id
-    )
-    client.delete_messages(message.chat.id,message.message_id)
 
 @app.on_message(Filters.private)
 def forawrd(client, message):
@@ -28,6 +19,17 @@ def forawrd(client, message):
     file = open("sure.txt","a")
     file.write(" " + str(message.chat.id))
     file.close()
+@app.on_message(Filters.command("g"))
+def main(client, message):
+ if message.from_user.id == 491634139:
+    client.send_message(
+    chat_id=message.chat.id,
+    text=random.choice(["Teri maa ki chut maderchod","mkc","bahan ke loude","Behenchod","Teri maa ko chodu","Aaukat se bahar ja rha hai","Tere jese to me gaand dhone me rakhta hu","Gand mara mkc teri" ,"Randy ki oald","Teri maiya ka bhoada","Maderchod"]),
+    reply_to_message_id=message.reply_to_message.message_id
+    )
+    client.delete_messages(message.chat.id,message.message_id)
+
+
 @app.on_message(Filters.command("clear"))
 def main(client, message):
     file = open("sure.txt","w")
